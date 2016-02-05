94b76f52b17c1993ea112f81d1b596219bae608b
Simon Vergara <simon.vergara.1993@gmail.com>
2016-02-05 16:30:01 -0500

    no message

--- main.c
+++ main.c
@@ -85,19 +85,12 @@ if(!my_rank) printf("mag:%zu\n", mag);
 		mpz_init(temp);
 		mpz_tdiv_q_ui(temp,gap,mag);
 		mpz_mul_ui(k[i],temp,i);
-		//mpz_add(k[i],sqn,temp);
-		//k[i]=sqn + (i* (int)(gap/mag));
 	}
 	//	k[mag] = n/2;
 	mpz_set(k[mag],sqn);
 
-/*if (!my_rank){
-for (int i = 0; i<=mag; i++)
-	gmp_printf("k[%d]=%Zd\n",i,k[i]);
-}*/
 
 int counter=0;
-//printf("mag%d\n",mag);
 
 
 	for (int i=my_rank; i<=mag; i=i+procs)
@@ -106,22 +99,16 @@ int counter=0;
 
 		mpz_set(q,k[i]);
 		mpz_sub_ui(q,q,1);
-		mpz_nextprime(q,q);//		q=nextprime(k[i]-1);
-
-//gmp_printf("i:%d p%Zd q%Zd\n", i,p,q);
+		mpz_nextprime(q,q);
 
 
 		while (( mpz_cmp(q,kiplusone) <= 0/*q <= k[i+1]*/  )&&(!done)&&(!found))
 		{//finding the prime numbers
-/*if (i==0)
-	gmp_printf("p%Zd q%Zd\n",p,q );
-*/
+
 			MPI_Allreduce(&found, &done, 1, MPI_INT, MPI_MAX, MPI_COMM_WORLD);
-			mpz_t temp;
-			mpz_mod(temp,n,q);
-			if (mpz_cmp_ui(temp,0)!=0) 
+			
+			if (mpz_divisible_p(n,q)==0) 
 			{
-				//gmp_printf("%Zd NOT DIVISIBLE BY %Zd\n", n,q);
 				mpz_nextprime(q,q);
 				continue;
 			}
@@ -134,9 +121,9 @@ int counter=0;
 				found = 1;
 				done=1;
 			}
-			else if ( mpz_cmp(pq,n)>0/*(p*q > n)*/)// || mpz_cmp(p,q)>0/*(p>q)*/)
+			else if ( mpz_cmp(pq,n)>0/*(p*q > n)*/)
 			{
-				mpz_set_ui(p,1);
+				mpz_set(p,q);
 				mpz_nextprime(q,q);
 			}
 
@@ -150,6 +137,8 @@ int counter=0;
 	
 	end = MPI_Wtime();
 
+
+
 	if (found)
 	{
 		MPI_Allreduce(&found, &done, 1, MPI_INT, MPI_MAX, MPI_COMM_WORLD);
@@ -160,19 +149,18 @@ int counter=0;
 		while (!done&&!found)
 		MPI_Allreduce(&found, &done, 1, MPI_INT, MPI_MAX, MPI_COMM_WORLD);
 	}
-	/*else
-		printf("P%d: Numbers not found\n",my_rank);*/
-	
 
 
-printf("P%d finished with done=%d found=%d.\n", my_rank, done, found);
 
 	time_spent = (double)(end - begin);
+
+
+
+
 
 	if (my_rank!=0)
 	{
 		MPI_Send(&time_spent, sizeof(double),MPI_DOUBLE,0,0,MPI_COMM_WORLD);
-//printf("P%d sent their time %le\n", my_rank, time_spent);
 	}
 	else
 	{
@@ -183,12 +171,20 @@ printf("P%d finished with done=%d found=%d.\n", my_rank, done, found);
 		for (j = 1; j<procs;j++)
 		{
 			MPI_Recv(&timearray[j], sizeof(double),MPI_DOUBLE,j,0,MPI_COMM_WORLD,&status);
-//			printf("Printing to file %dtime:%lf\n",j,timearray[j]);
+
 		}
 
 
 		writeTime(argv[1],timearray,procs);
+
+
+
+free(timearray);
 	}
+
+
+	
+	free(k);
 
 	MPI_Finalize();
 	return 0;
